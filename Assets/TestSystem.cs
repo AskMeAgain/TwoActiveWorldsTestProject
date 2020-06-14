aa8cc4bf62194a28ddcc1157d81f123c09c6c782
Kelvin Petry <test@test.de>
2020-06-14 21:33:57 +0200

    Small changes

--- Assets/TestSystem.cs
+++ Assets/TestSystem.cs
@@ -12,24 +12,27 @@ public class TestSystem : ComponentSystem
     protected override void OnCreate()
     {
         var settings = new GameObjectConversionSettings(EntityManager.World,
-            GameObjectConversionUtility.ConversionFlags.AssignName,
-            new BlobAssetStore());
+            GameObjectConversionUtility.ConversionFlags.AssignName, new BlobAssetStore());
 
         var cube = GameObjectConversionUtility.ConvertGameObjectHierarchy(Resources.Load<GameObject>("cube"), settings);
         EntityManager.SetName(cube, "Prefab:" + EntityManager.World.Name);
 
-        var entity2 = EntityManager.Instantiate(cube);
-        EntityManager.SetName(entity2, "Real Cube:" + EntityManager.World.Name);
+        for (int i = 0; i < 100; i++)
+        {
+            var entity2 = EntityManager.Instantiate(cube);
+            EntityManager.SetName(entity2, "Real Cube:" + EntityManager.World.Name);
 
-        if (EntityManager.World.Name.Equals("World 1"))
-        {
-            EntityManager.SetComponentData(entity2, new Translation() {Value = new float3(3, 0, 0)});
-            EntityManager.SetComponentData(cube, new Translation() {Value = new float3(3, 0, 0)});
-        }
-        else
-        {
-            EntityManager.SetComponentData(entity2, new Translation() {Value = new float3(0, 0, 0)});
-            EntityManager.SetComponentData(cube, new Translation() {Value = new float3(0, 0, 0)});
+            var positionValue = i + i * 0.4f;
+            if (EntityManager.World.Name.Equals("World 1"))
+            {
+                EntityManager.SetComponentData(entity2, new Translation() {Value = new float3(3, 0, positionValue)});
+                EntityManager.SetComponentData(cube, new Translation() {Value = new float3(3, 0, positionValue)});
+            }
+            else
+            {
+                EntityManager.SetComponentData(entity2, new Translation() {Value = new float3(0, 0, positionValue)});
+                EntityManager.SetComponentData(cube, new Translation() {Value = new float3(0, 0, positionValue)});
+            }
         }
     }
 
